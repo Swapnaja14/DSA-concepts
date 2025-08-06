4110544d4f65c91dff18c472ef431d3a2ab1102d
Swapnaja Yadav <swapnajayadav2021@gmail.com>
2025-08-06 23:28:30 +0530

    Learn implementation of Stack using ArrayList and LinkedList and Create Stack using Java Collection Framework

---
+++ Stacks/Stack using Java Collection Framework/stack.java
@@ -0,0 +1,17 @@
+// package Stacks.Stack using Java Collection Framework;
+
+import java.util.*;
+
+public class stack {
+    public static void main(String[] args) {
+        Stack<Integer> s = new Stack();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+
+        while (!s.isEmpty()) {
+            System.out.println(s.peek());
+            s.pop();
+        }
+    }
+}
